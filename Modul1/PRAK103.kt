3d34c9423addf6673f909eaad575e1c0210ea522
Mr24 <101628346+Mr24E7@users.noreply.github.com>
2022-03-23 08:34:33 +0800

    Update PRAK103.kt

--- Modul1/PRAK103.kt
+++ Modul1/PRAK103.kt
@@ -3,16 +3,16 @@ package Modul1
 fun main() {
     print("Input Bilangan : ")
     var x:Int = Integer.valueOf(readLine())
-    val y = x
-    var z = 5
-    for (i in 1..z){
+    val a = x
+    var b = 5
+    for (i in 1..b){
         if (x % 2 == 0 || x % 3 == 0) {
             print(x)
             print(" ")
-            x += y
+            x += a
         } else {
-            x += y
-            z += 1
+            x += a
+            b += 1
         }
     }
 }
